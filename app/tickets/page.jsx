cf31f629d11036e42b03036386c639167f28a74c
Mg Chit <chitlunh@gmail.com>
2023-08-28 22:52:50 +0630

    [Added] Ticket List

---
+++ app/tickets/page.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react"
+import TicketList from "./TicketList"
+import Loading from "../loading"
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Currently open ticket</small></p>
+        </div>
+      </nav>
+      <Suspense fallback={<Loading/>}>
+        <TicketList />
+      </Suspense>
+    </main>
+  )
+}
